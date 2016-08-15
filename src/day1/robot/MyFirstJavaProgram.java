3337ef07f7300c5cce42e65e5575700ac7836f33
JackShu1 <league@Mac-mini-2.local>
2016-08-15 15:03:02 -0700

    First day of computer class.

--- src/day1/robot/MyFirstJavaProgram.java
+++ src/day1/robot/MyFirstJavaProgram.java
@@ -6,10 +6,17 @@ import org.jointheleague.graphical.robot.Robot;
 public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
-		// START HERE
-
+		// START HERE 
 	
+	Robot Jack = new Robot();
+Jack.setRandomPenColor();
 		
-		
+Jack.penDown();	
+for (int i = 0; i < 4; i++) {
+	Jack.move(3);		
+Jack.turn(-300);
+}
+	
+	
 	}
 }
